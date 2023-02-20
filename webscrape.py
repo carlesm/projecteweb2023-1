74166771e7ba943c5c53fb691eb488de9198f8b3
Carles Mateu <carlesm@carlesm.com>
2023-02-20 08:43:53 +0000

    Parse html

--- webscrape.py
+++ webscrape.py
@@ -1,5 +1,6 @@
 
 import urllib3
+import bs4
 
 
 class WebScrape(object):
@@ -16,11 +17,26 @@ class WebScrape(object):
         # guardar html resposta
         self.html = resposta.data.decode("utf-8")
         
+    def parse_bs4(self):
+        soup = bs4.BeautifulSoup(self.html, features="html.parser")
+        div_main = soup.find_all('div', attrs={"id":"main"})[0]
+        self.data = div_main.find_all('pre')[0]
+        
+    def extract_data(self):
+        self.data = self.data.text
+
+    def parse_html(self):
+        # parse bs4
+        self.parse_bs4()
+        # extraure dades html
+        self.extract_data()
+
 
     def get_data(self):
         # descarregar web
         self.get_web()
         # llegir html
+        self.parse_html()
         # retornar dades
         pass
 
