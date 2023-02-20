1f0b8173c316626f2a4ffcaa1bc6d5da5ea1c33c
Carles Mateu <carlesm@carlesm.com>
2023-02-20 09:16:33 +0000

    Getting XML

--- apiclient1.py
+++ apiclient1.py
@@ -1,13 +1,26 @@
 
+import urllib.request
+import xmltodict
 
 class ArxivClient(object):
     def __init__(self) -> None:
         self.url = "http://export.arxiv.org/api/query?start=0&max_results=1&search_query=all:"
         pass
 
-    def get_data(self):
+    def query_arxiv(self, query):
+        url_query = urllib.request.urlopen(self.url+query)
+        xml_data = url_query.read().decode("utf-8")
+        return xml_data
+    
+    def convert_xml(self, results):
+        dades = xmltodict.parse(results)
+        return dades
+
+    def get_data(self, query):
         # consultar API arxiv
+        result_arxiv = self.query_arxiv(query)
         # convertir xml -> dades
+        dades = self.convert_xml(result_arxiv)
         # reduir dades
         # return dades
         pass
