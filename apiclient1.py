651a96d15d744126703c4bf6d23775fa631629b9
Carles Mateu <carlesm@carlesm.com>
2023-02-20 09:32:43 +0000

    Done

--- apiclient1.py
+++ apiclient1.py
@@ -1,10 +1,11 @@
 
 import urllib.request
 import xmltodict
+import pprint
 
 class ArxivClient(object):
     def __init__(self) -> None:
-        self.url = "http://export.arxiv.org/api/query?start=0&max_results=1&search_query=all:"
+        self.url = "http://export.arxiv.org/api/query?start=0&max_results=2&search_query=all:"
         pass
 
     def query_arxiv(self, query):
@@ -16,17 +17,32 @@ class ArxivClient(object):
         dades = xmltodict.parse(results)
         return dades
 
+    def extract_data(self, data):
+        results = []
+        print(data)
+        print(type(data))
+        for e in data["feed"]["entry"]:
+            title = e["title"]
+            if len(e["author"]) > 1:
+                authors = [a["name"] for a in e["author"]]
+                authors = ",".join(authors)
+            else:
+                authors = e['author']["name"]
+            results.append((title,authors))
+        return results
+
     def get_data(self, query):
         # consultar API arxiv
         result_arxiv = self.query_arxiv(query)
         # convertir xml -> dades
         dades = self.convert_xml(result_arxiv)
         # reduir dades
+        resultats = self.extract_data(dades)
         # return dades
-        pass
+        return resultats
 
 
 if __name__=="__main__":
     client = ArxivClient()
     dades = client.get_data("transformers")
-    print(dades)
+    pprint.pprint(dades)
