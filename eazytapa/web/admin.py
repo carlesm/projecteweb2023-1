1ffa9197abd3d82b4cb7b4a1f7600c04395f3c72
Carles Mateu <carlesm@carlesm.com>
2023-02-27 09:57:03 +0100

    Added 2 models

--- eazytapa/web/admin.py
+++ eazytapa/web/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from web.models import Bar, Tapa
 # Register your models here.
+
+admin.site.register(Bar)
+admin.site.register(Tapa)
--- eazytapa/web/models.py
+++ eazytapa/web/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Bar(models.Model):
+    name = models.CharField(max_length=200)
+
+class Tapa(models.Model):
+    name = models.CharField(max_length=200)
+    bar = models.ForeignKey(Bar, on_delete=models.CASCADE)
+
+    
