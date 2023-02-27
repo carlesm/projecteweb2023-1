f5e5dc323a2b7ad08e5737037ad15645a73cd97a
Carles Mateu <carlesm@carlesm.com>
2023-02-27 10:02:38 +0100

    Added models to admin

--- eazytapa/web/models.py
+++ eazytapa/web/models.py
@@ -5,8 +5,13 @@ from django.db import models
 class Bar(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self) -> str:
+        return self.name
+
 class Tapa(models.Model):
     name = models.CharField(max_length=200)
     bar = models.ForeignKey(Bar, on_delete=models.CASCADE)
 
-    
+
+    def __str__(self) -> str:
+        return self.name+"->"+self.bar.name
